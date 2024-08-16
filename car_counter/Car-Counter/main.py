465f2d4b370d77543cfd162a0d4060b2087c4903
AnupaAlwis <isurajithalwis@gmail.com>
2024-08-17 02:30:40 +0530

    updates

--- car_counter/Car-Counter/main.py
+++ car_counter/Car-Counter/main.py
@@ -31,7 +31,7 @@ mask = cv2.imread("../Resources/mask.png")  # Import the created mask
 # tracker
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 limits = [400, 297, 673, 297]  # selected points to draw the line on the image
-totalCount = []
+totalCount = [] # empty list to store the count of vehicles
 
 while True:
     success, img = cap.read()
@@ -56,8 +56,7 @@ while True:
             cls = int(box.cls[0])
             currentClass = classNames[cls]
 
-            if (
-                    currentClass == "motorbike" or currentClass == "car" or currentClass == "bus" or currentClass == "truck") and conf > 0.4:
+            if (currentClass == "motorbike" or currentClass == "car" or currentClass == "bus" or currentClass == "truck") and conf > 0.4:
                 # cvzone.putTextRect(img,f"{currentClass} {conf}",(max(0,x1),max(35,y1)), scale = 2,thickness = 3, offset = 3)
                 # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt= 2, colorR=(255, 0, 255))
                 currentArray = np.array([x1, y1, x2, y2, conf])  # store the detections
@@ -70,8 +69,7 @@ while True:
         x1, y1, x2, y2, id = result  # result consist of coordinates of the cox and id
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2,
-                          colorR=(255, 0, 0))  # draw the rectangle of the sorted detector
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2,colorR=(255, 0, 0))  # draw the rectangle of the sorted detector
         cvzone.putTextRect(img, f"{id}", (max(0, x1), max(35, y1)), scale=2, thickness=3, offset=10)  # show id number
         cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
         cv2.circle(img, (int(cx), int(cy)), 5, (255, 0, 255), cv2.FILLED)
